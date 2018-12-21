1e6fc8589b7eea85cce87a75d312faaee784aa78
Yehonal <yehonal.azeroth@gmail.com>
2018-12-21 20:18:25 +0100

    red links feature improvements

--- docs/assets/js/red-links.js
+++ docs/assets/js/red-links.js
@@ -9,12 +9,20 @@
             if (this.hostname != window.location.hostname)
                 return;
 
+            var ext = this.href.split('.').pop().split(/\#|\?/)[0];
+
+            // [Performance tip] pessimistic condition based on the fact that
+            // markdown files are automatically converted in html
+            // if they are part of the wiki (the real check is right below)
+            if (ext.toLowerCase() == "md" || ext.toLowerCase() == "markdown")
+                $(this).css('color', 'red');
+
             var that = this;
             $.ajax({
                 type: 'HEAD',
                 url: this.href,
                 success: function () {
-
+                    $(that).css('color', '');
                 },
                 error: function (xhr, ajaxOptions, thrownError) {
                     if (xhr.status == 404) {
