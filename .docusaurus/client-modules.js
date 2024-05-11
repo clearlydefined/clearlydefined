97bcbe13ea1979515d3eb2b2f20b8a66241280d5
E. Lynette Rayle <elrayle@github.com>
2024-05-11 11:38:45 -0400

    delete .docusaurus dir and contents before adding to .gitignore

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/home/nick/cd_docs2/source/node_modules/infima/dist/css/default/default.css"),
-  require("/home/nick/cd_docs2/source/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/home/nick/cd_docs2/source/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/home/nick/cd_docs2/source/src/css/custom.css"),
-];
